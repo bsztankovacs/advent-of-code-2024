27a87e6938b69931b38fd21c067e478e3e362ebd
Barnabás Sztán-Kovács <bsztankovacs@gmail.com>
2024-12-11 10:26:21 +0100

    add day2 2/2 typescript solution

--- day-2/main.js
+++ day-2/main.js
@@ -36,11 +36,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fs = __importStar(require("fs"));
 // Read input txt file
 const input = fs.readFileSync('input.txt', 'utf-8').split('\n');
-// Function to check if the report is safe, accepts a string and returns a boolean
-function isSafe(report) {
+// Function to check if the report is safe. 
+// Accepts a string and if the report is safe, returns true, otherwise false and the index of the unsafe digit
+function isSequenceSafe(digits) {
     // Split the report string, convert to integers
-    const digits = report.split(' ').map(Number);
-    let problemDampener = 0;
     let isIncreasing;
     // Check the first four digits to determine the direction of the sequence. If there is more asc than desc, the sequence is increasing
     let ascCount = 0;
@@ -54,35 +53,16 @@ function isSafe(report) {
         }
     }
     isIncreasing = ascCount > descCount;
+    let unsafeIndex = -1;
     // Iterate through the digits, check for each digit if matches the original direction, and checking if the difference is between bounds
     for (let i = 1; i < digits.length; i++) {
         let wasSafeStep = ifSafeStep(digits[i - 1], digits[i]);
         if (!wasSafeStep) {
-            console.log('Not safe step, because direction:', isIncreasing, 'digits:', digits[i - 1], digits[i]);
-            if (problemDampener > 0) {
-                return false;
-            }
-            // check if the following digit is safe as well
-            if (i + 1 < digits.length) {
-                // check if the following digit is safe with the first or the second digit of the current pair
-                let isSafeWithFirstNum = ifSafeStep(digits[i - 1], digits[i + 1]);
-                let isSafeWithSecondNum = ifSafeStep(digits[i], digits[i + 1]);
-                if (!isSafeWithFirstNum && !isSafeWithSecondNum) {
-                    return false;
-                }
-                // replace the second digit with the first digit if it is safe
-                if (isSafeWithFirstNum) {
-                    digits[i] = digits[i - 1];
-                }
-            }
-            problemDampener++;
-        }
-        if (problemDampener > 1) {
-            return false;
+            unsafeIndex = i;
+            return [false, unsafeIndex];
         }
     }
-    console.log('Safe report:', report);
-    return true;
+    return [true, unsafeIndex];
     function ifSafeStep(firstNum, secondNum) {
         if (isIncreasing) {
             if (!(secondNum > firstNum && secondNum - firstNum <= 3)) {
@@ -97,9 +77,27 @@ function isSafe(report) {
         return true;
     }
 }
+function isReportSafe(report) {
+    let reportDigits = report.split(' ').map(Number);
+    let [isReportSafe, unsafeIndex] = isSequenceSafe(reportDigits);
+    if (isReportSafe) {
+        return true;
+    }
+    // If the report is not safe, create a new report with the unsafe digit removed - two options
+    // Create a deep copy of the report
+    let reportDigits2 = reportDigits.slice();
+    let reportDigits1 = reportDigits.slice();
+    // Remove the unsafe digit
+    reportDigits1.splice(unsafeIndex, 1);
+    reportDigits2.splice(unsafeIndex - 1, 1);
+    // Check if the corrected report is safe
+    let [isCorrectedReportSafe1] = isSequenceSafe(reportDigits1);
+    let [isCorrectedReportSafe2] = isSequenceSafe(reportDigits2);
+    return isCorrectedReportSafe1 || isCorrectedReportSafe2;
+}
 // Function to solve the problem
 function solve(input) {
-    let safeReports = input.filter(isSafe);
+    let safeReports = input.filter(isReportSafe);
     console.log(safeReports.length);
 }
 // Function for manual testing, which listens on the console and calls the solve function with the input
@@ -111,9 +109,9 @@ function manualTest() {
     });
     // for each line, call the solve function with the input
     readline.on('line', (line) => {
-        console.log(isSafe(line));
+        console.log(isReportSafe(line));
     });
 }
 // Call the solve function with the input
-// solve(input);
-manualTest();
+solve(input);
+// manualTest();
--- day-2/main.ts
+++ day-2/main.ts
@@ -3,11 +3,10 @@ import * as fs from 'fs';
 // Read input txt file
 const input = fs.readFileSync('input.txt', 'utf-8').split('\n');
 
-// Function to check if the report is safe, accepts a string and returns a boolean
-function isSafe(report: string): boolean {
+// Function to check if the report is safe. 
+// Accepts a string and if the report is safe, returns true, otherwise false and the index of the unsafe digit
+function isSequenceSafe(digits: number[]): [boolean, number] {
     // Split the report string, convert to integers
-    const digits = report.split(' ').map(Number);
-    let problemDampener = 0;
 
     let isIncreasing: boolean;
 
@@ -23,37 +22,19 @@ function isSafe(report: string): boolean {
     }
     
     isIncreasing = ascCount > descCount;
+    let unsafeIndex = -1;
 
     // Iterate through the digits, check for each digit if matches the original direction, and checking if the difference is between bounds
     for (let i = 1; i < digits.length; i++) {
         let wasSafeStep = ifSafeStep(digits[i - 1], digits[i]);
         if (!wasSafeStep) {
-            console.log('Not safe step, because direction:', isIncreasing, 'digits:', digits[i - 1], digits[i]);
-            if (problemDampener > 0) {
-                return false;
-            }
-            // check if the following digit is safe as well
-            if (i + 1 < digits.length) {
-                // check if the following digit is safe with the first or the second digit of the current pair
-                let isSafeWithFirstNum = ifSafeStep(digits[i - 1], digits[i + 1]);
-                let isSafeWithSecondNum = ifSafeStep(digits[i], digits[i + 1]);
-                if (!isSafeWithFirstNum && !isSafeWithSecondNum) {
-                    return false;
-                }
-                // replace the second digit with the first digit if it is safe
-                if (isSafeWithFirstNum) {
-                    digits[i] = digits[i - 1];
-                }
-            }
-            problemDampener++;
-        }
-        if (problemDampener > 1) {
-            return false;
+            unsafeIndex = i;
+            return [false, unsafeIndex];
         }
     }
-    console.log('Safe report:', report);
-    return true;
 
+    return [true, unsafeIndex];
+    
     function ifSafeStep(firstNum: number, secondNum: number): boolean {
         if (isIncreasing) {
             if (!(secondNum > firstNum && secondNum - firstNum <= 3)) {
@@ -68,9 +49,31 @@ function isSafe(report: string): boolean {
     }
 }
 
+function isReportSafe(report: string): boolean {
+    let reportDigits = report.split(' ').map(Number);
+    let [isReportSafe, unsafeIndex] = isSequenceSafe(reportDigits);
+    
+    if (isReportSafe) { return true; }
+    
+    // If the report is not safe, create a new report with the unsafe digit removed - two options
+    // Create a deep copy of the report
+    let reportDigits2 = reportDigits.slice();
+    let reportDigits1 = reportDigits.slice();
+
+    // Remove the unsafe digit
+    reportDigits1.splice(unsafeIndex, 1);
+    reportDigits2.splice(unsafeIndex - 1, 1);
+    
+    // Check if the corrected report is safe
+    let [isCorrectedReportSafe1] = isSequenceSafe(reportDigits1);
+    let [isCorrectedReportSafe2] = isSequenceSafe(reportDigits2);
+
+    return isCorrectedReportSafe1 || isCorrectedReportSafe2;
+}
+
 // Function to solve the problem
 function solve(input: string[]): void {
-    let safeReports = input.filter(isSafe);
+    let safeReports = input.filter(isReportSafe);
     console.log(safeReports.length);
 }
 
@@ -84,10 +87,10 @@ function manualTest(): void {
 
     // for each line, call the solve function with the input
     readline.on('line', (line: string) => {
-        console.log(isSafe(line));
+        console.log(isReportSafe(line));
     });
 }
 
 // Call the solve function with the input
-// solve(input);
-manualTest();
+solve(input);
+// manualTest();
